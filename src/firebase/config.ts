8627c15857a7af2e5604992eb582d0918f5bf648
options didi <didioptions@gmail.com>
2025-10-16 21:49:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5618869838-18486",
+  "appId": "1:1059962490351:web:6ed75997aad9ad43afba1a",
+  "apiKey": "AIzaSyBMMdB5UEPLP6LrWKHywytJhgUVEY18kdQ",
+  "authDomain": "studio-5618869838-18486.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1059962490351"
+};
