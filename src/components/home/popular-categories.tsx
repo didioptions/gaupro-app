4335708af337b11029088cb3e43b1c41a7d9b235
options didi <didioptions@gmail.com>
2025-10-27 21:34:36 +0000

    can u put security where is cleaning services , and cleaning in to secur

--- src/components/home/popular-categories.tsx
+++ src/components/home/popular-categories.tsx
@@ -10,11 +10,11 @@ import { CategoryImages } from "@/lib/category-images";
 const popularServices = [
   { name: "Builders", href: "/services/builders", imageId: "builders-image" },
   { name: "Movers", href: "/services/movers", imageId: "movers-image" },
-  { name: "Cleaning Services", href: "/services/cleaning", imageId: "cleaning-image" },
+  { name: "Security", href: "/services/security", imageId: "security-image" },
   { name: "Website Designers", href: "/services/website-designers", imageId: "website-designers-image" },
   { name: "Architects", href: "/services/architects", imageId: "architects-image" },
   { name: "DSTV Installers", href: "/services/dstv-installers", imageId: "dstv-installers-image" },
-  { name: "Security", href: "/services/security", imageId: "security-image" },
+  { name: "Cleaning Services", href: "/services/cleaning", imageId: "cleaning-image" },
   { name: "Caterers", href: "/services/caterers", imageId: "caterers-image" },
 ];
 
