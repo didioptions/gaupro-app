08170f4003325962199feaaf0a131b5f1cdaf689
options didi <didioptions@gmail.com>
2025-10-29 14:22:17 +0000

    ReferenceError: DialogTrigger is not defined     at RequestQuoteDialog (

--- src/components/request-quote-dialog.tsx
+++ src/components/request-quote-dialog.tsx
@@ -7,6 +7,7 @@ import {
   DialogContent,
   DialogHeader,
   DialogTitle,
+  DialogTrigger,
 } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 import { ArrowLeft } from 'lucide-react';
