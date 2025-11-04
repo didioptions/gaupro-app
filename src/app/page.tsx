dc1e924212382370f3f7d614525d6592d7269889
options didi <didioptions@gmail.com>
2025-11-04 10:58:24 +0000

    here is where i what change,  where i mark in red pen , i need this only

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 
 import Hero from '@/components/home/hero';
-import HowItWorks from '@/components/home/how-it-works';
 import PopularCategories from '@/components/home/popular-categories';
 import ProCta from '@/components/home/pro-cta';
-import Testimonials from '@/components/home/testimonials';
 import Header from '@/components/layout/header';
 import Footer from '@/components/layout/footer';
 import GrowClientBaseCta from '@/components/layout/grow-client-base-cta';
@@ -15,8 +13,6 @@ export default function Home() {
       <main className="flex-grow">
         <Hero />
         <PopularCategories />
-        <HowItWorks />
-        <Testimonials />
         <ProCta />
         <GrowClientBaseCta />
       </main>
