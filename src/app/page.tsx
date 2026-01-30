65161df993e0421ca9967a64d84771c8d65611fe
options didi <didioptions@gmail.com>
2026-01-30 22:57:58 +0000

    please bring back what here at bottom of page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,23 @@ import PopularCategories from '@/components/home/popular-categories';
 import ProCta from '@/components/home/pro-cta';
 import Testimonials from '@/components/home/testimonials';
 import WhyHire from '@/components/home/why-hire';
+import Header from '@/components/layout/header';
+import Footer from '@/components/layout/footer';
 
 export default function HomePage() {
   return (
     <>
-      <Hero />
-      <WhyHire />
-      <FeaturedProfessionals />
-      <HowItWorks />
-      <PopularCategories />
-      <Testimonials />
-      <ProCta />
+      <Header />
+      <main>
+        <Hero />
+        <WhyHire />
+        <FeaturedProfessionals />
+        <HowItWorks />
+        <PopularCategories />
+        <Testimonials />
+        <ProCta />
+      </main>
+      <Footer />
     </>
   );
 }
