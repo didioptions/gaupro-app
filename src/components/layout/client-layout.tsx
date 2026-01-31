2f1135376f931e0aff64fab43d822ee45cbf9779
options didi <didioptions@gmail.com>
2026-01-31 19:05:07 +0000

    it must stay , where it was , so anyone can use it

--- src/components/layout/client-layout.tsx
+++ src/components/layout/client-layout.tsx
@@ -3,6 +3,7 @@
 import { FirebaseClientProvider } from '@/firebase';
 import { Toaster } from '@/components/ui/toaster';
 import CookieConsentBanner from '@/components/layout/cookie-consent-banner';
+import PublicChatWidget from './public-chat-widget';
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -10,6 +11,7 @@ export default function ClientLayout({ children }: { children: React.ReactNode }
       <div className="flex-grow">{children}</div>
       <Toaster />
       <CookieConsentBanner />
+      <PublicChatWidget />
     </FirebaseClientProvider>
   );
 }
