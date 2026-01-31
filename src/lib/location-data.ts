b8117c30717e98d76516f3e9aebf9abdf3900254
options didi <didioptions@gmail.com>
2026-01-31 21:10:41 +0000

    You are an expert Firebase marketplace analysis agent.
    MODE:
    DRY-RUN ON

--- src/lib/location-data.ts
+++ src/lib/location-data.ts
@@ -1,9 +1,9 @@
 
 export const cityExpansionMap: Record<string, string[]> = {
-  johannesburg: ['johannesburg', 'sandton', 'randburg', 'roodepoort', 'midrand', 'soweto', 'alberton', 'boksburg', 'benoni', 'germiston', 'kempton-park', 'edenvale', 'bryanston', 'fourways', 'rosebank'],
-  pretoria: ['pretoria', 'centurion', 'akasia', 'bronkhorstspruit'],
+  johannesburg: ['johannesburg', 'sandton', 'randburg', 'roodepoort', 'midrand', 'soweto', 'alberton', 'boksburg', 'benoni', 'germiston', 'kempton-park', 'edenvale', 'bryanston', 'fourways', 'rosebank', 'vereeniging', 'vanderbijlpark', 'meyerton'],
+  pretoria: ['pretoria', 'centurion', 'akasia', 'bronkhorstspruit', 'tembisa'],
   'cape-town': ['cape-town', 'bellville', 'durbanville', 'mitchells-plain', 'goodwood', 'blackheath', 'atlantis', 'kraaifontein', 'kuils-river', 'table-view', 'stellenbosch', 'paarl', 'somerset-west'],
-  durban: ['durban', 'amanzimtoti', 'ballito', 'chatsworth', 'hillcrest', 'kloof', 'phoenix', 'pinetown', 'umhlanga'],
+  durban: ['durban', 'amanzimtoti', 'ballito', 'chatsworth', 'hillcrest', 'kloof', 'phoenix', 'pinetown', 'umhlanga', 'umlazi'],
   'port-elizabeth': ['port-elizabeth', 'despatch'],
   'east-london': ['east-london', 'gonubie'],
   bloemfontein: ['bloemfontein'],
